29e5e7d375407eeda71d20aa2eb208caccc7adf8
Kamiko Loss <info@gloxi.net>
2024-01-09 01:59:49 +0900

    layouts, pages の骨組みを作成した

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="min-h-screen">
+    <header>
+      <div class="text-center py-4">
+        <h1 class="font-bold text-2xl">GLOXINIA Sites Sample</h1>
+        <p>GLOXINIA の Web サイト制作のサンプルです。</p>
+      </div>
+    </header>
+    <main>
+      <div class="w-full max-w-3xl mx-auto">
+        <slot />
+      </div>
+    </main>
+    <footer>
+      <div class="text-center text-sm py-4">
+        <p>FOOTER</p>
+      </div>
+    </footer>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="text-center">
+    <p>Hello world!</p>
+  </div>
+</template>
